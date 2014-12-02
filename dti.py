8e99fa1d59045e3a60e12a120cc4f9f6189326d6
shannon buckley <shannon.buckley@ucsf.edu>
2014-12-02 10:16:17 -0800

    Starter Kit for DTI pypeline 2.0...

--- dti.py
+++ dti.py
@@ -3,6 +3,66 @@
 __author__ = 'Shannon Buckley', 12/2/14
 """
 
-import os, sys
+#import CINDBOILERPLATE
+import os
+import sys
+import shutil
 from os import path
 
+VERSION = '1.0.0'
+
+PROCESS_NAME = 'DTI Processing'
+
+
+# helpers
+def get_project_path(project_name):
+    pass
+
+
+def get_project_config(project_name):
+    pass
+
+
+class DtiProcessing():
+    """Base class for all dti processing steps"""
+    pass
+
+
+class StageDWI():
+    """Use dcm2nii to assemble 4D DWI volume (nii)"""
+    pass
+
+
+class MaskB0():
+    """Use fsl to queue up new masks for editing via BET"""
+    pass
+
+
+class CoregisterToStructural():
+    """Concatenate reg of dwi to t2 and t2 to t1: get dwi to t1"""
+    pass
+
+
+class EddyCurrentCorrection():
+    """Run Eddy Correction... Project config? Needs: 4D DWI and b0 mask"""
+    pass
+
+
+class EPIGeometricDistortionCorrection():
+    """Run EPI correction... Project config? Needs EddyCorr, DWI to T2 coreg, params"""
+    pass
+
+
+class EstimateTensors():
+    """Apply b0 mask to DWI and run tensor estimations. Project Config: Teem or FSL?"""
+    pass
+
+
+class QualityAssurance():
+    """Use fsl to bring up 1) T2 w/ EPIcorr (b0) olay; 2) T1 w/ FA olay"""
+    pass
+
+
+def main():
+    pass
+# main
