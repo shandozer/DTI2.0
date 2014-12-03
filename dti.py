45d5ac6bde07215885ae7ec604eb7500f3f1ac15
Shannon Buckley <buckley.shannont@gmail.com>
2014-12-03 15:11:35 -0800

    Fleshing out more details…

--- dti.py
+++ dti.py
@@ -24,56 +24,57 @@ def get_project_config(project_name):
 
 
 class DtiProcessing():
-    """Base class for all dti processing steps.
-    1) Stage dicoms to make 4D DWI volume (nii.gz for all formats unless otherwise stated)
+    """Base class for all dti processing steps. All file formats .nii.gz unless otherwise stated.
+    1) Stage dicoms to make 4D DWI volume (all frames + b0)
     2) MaskB0. Runs BET and pauses for manual work & associated plog entry = Pass/Done.
-    3) Coregistration to Structural. This WILL be needed later for EPI. Get it over with.
-        Take DWI to T2 (via masked b0, and requires a masked T2 (via T2-ICV? config). Outputs many intermediates.
-
+    3) Coregistration to Structural. Requires: Step 2 plog = Pass/Don AND T2-BrainMask (T2-ICV).
+        -Take DWI to T2 (via masked b0, via T2-ICV (config). Outputs .mat solving for T2->T1, B0->T2, concatenated B0->T1, and inverses.
+        -Could also be made to take aparc+aseg from FS for starter-T2-mask (for passing FS QC cases).
     4) Eddy Correction: either FSL or Fletcher (config). Input masked b0 and 4D DWI.
-    5) EPI Correction: assuming you want this (config) you can use FSL or Fletcher (config). In: EddyCorr DWI,
-    out: EPIcorrection DWI data (a warped, T2-like-space, essentially), the warp fields.
+    5) EPI Correction: assuming you want this (config) you can use FSL or Fletcher (config).
+        In: EddyCorr DWI,
+        Out: EPIcorrection DWI data (a warped, T2-like-space, essentially), the warp fields.
     6) Tensor Estimation: FSL or Teem (config). In: b0 masked, Eddy Corrected & EPI-corrected (or not. config) DWI.
     Out: many files.
-    7) Do some QC... FSLview is great. 1) assess EPI-correciton quality ; 2) asses overall DTI quality (usually FA
+    7) Do some QC... FSLview is great. 1) assess EPI-correctiton quality ; 2) asses overall DTI quality (usually FA
     sufficient)
     8) config: package up a list of deliverables specific to that project & its collaborators?"""
     pass
 
 
-class StageDWI():
-    """Use dcm2nii to assemble 4D DWI volume (nii). In: raw dicoms. Out: 4D_DWI.nii.gz"""
+class StageDWI(DtiProcessing):
+    """Use dcm2nii to assemble 4D DWI volume (nii). In: raw dicoms. Out: 4D_DWI.nii.gz, contains all frames + b0"""
     pass
 
 
-class MaskB0():
-    """Use fsl to queue up new masks (BET) for editing. Input: b0 frame from 4D DWI. Out: maskedB0."""
+class MaskB0(DtiProcessing):
+    """Use fsl to queue up new masks (BET2) for editing. Input: b0 frame from 4D_DWI. Out: maskedB0."""
     pass
 
 
-class CoregisterToStructural():
+class CoregisterToStructural(DtiProcessing):
     """Concatenate reg of dwi to t2 and t2 to t1: get dwi to t1. In: DWI + mask, Structural + Mask(s). Out: .mat
     files for all registrations in both directions."""
     pass
 
 
-class EddyCurrentCorrection():
+class EddyCurrentCorrection(DtiProcessing):
     """Run Eddy Correction... Project config? In: 4D DWI and b0 mask. Out: EddyCorrected 4D DWI"""
     pass
 
 
-class EPIGeometricDistortionCorrection():
+class EPIGeometricDistortionCorrection(DtiProcessing):
     """Run EPI correction... Project config? In: EddyCorr DWI, DWI to T2 coreg, Fletcher params"""
     pass
 
 
-class EstimateTensors():
+class EstimateTensors(DtiProcessing):
     """Apply b0 mask to DWI and run tensor estimations. Project Config: Teem or FSL? In: 4D DWI, b0 Mask. Out: Many
     DTI related files."""
     pass
 
 
-class QualityAssurance():
+class QualityAssurance(DtiProcessing):
     """Use fsl to bring up 1) T2 w/ EPIcorr (b0) olay to verify EPI correction quality;
     2) T1 w/ FA olay to verify adequacy of MRI-space dti data (FA often the most critical)."""
     pass
@@ -82,6 +83,7 @@ class QualityAssurance():
 def main():
     """do all the things"""
     pass
+
 
 # REPOSITORY
 
